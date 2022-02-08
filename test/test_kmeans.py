4b64459ce375eb2a6f08561adb8514d69480a723
Grace Ramey <graceramey@pcd-ak2q05d-lt.ucsf.edu>
2022-02-07 23:54:10 -0800

    updated which unit tests I will implement in the comments

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -2,3 +2,19 @@
 import pytest
 import numpy as np
 from cluster import (KMeans, Silhouette)
+
+def test_edgeCase_clustering():
+    #test things like k=0, samples<k, large dimensionality (m), low dimensionality (m=1), very high k
+    
+    pass
+
+
+def test_clustering():
+    #check to make sure that a few points that should be clustered together are in fact assigned the same labels
+    pass
+
+
+def test_silhouette_scoring():
+    #check to make sure silhouette scores fall between -1 and 1
+    #check that silhouette scores incr. as k increases (maybe compare avg silhouette score for the data in different k cases)
+    pass
