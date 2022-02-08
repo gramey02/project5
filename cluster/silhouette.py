4906a6c1a77b9961857ec3b474dcc0611b571d4c
Grace Ramey <graceramey@pcd-ak2q05d-lt.ucsf.edu>
2022-02-07 19:06:55 -0800

    began silhouette scoreing method

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -8,6 +8,8 @@ class Silhouette:
             metric: str
                 the name of the distance metric to use
         """
+        #assign initial attribute
+        self.metric = metric
 
     def score(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         """
@@ -24,5 +26,19 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        #you have the input data and the labels for the input data
+        #silhouette score for data point i is the distance from the point to the closest cluster, minus the distance from the point to the other points in the same cluster
+        for i in range(0,X.shape(0)):
+            cur_label = y[i]
+        
+        
+        
+        #for each point
+            #get the label of that point
+            #get the distance between that point and the other points in the same label
+            #get the distance between that point and all other points not in the same label
+            #take average within non-same clusters to get separation score
+            #take min of separation scores
+        return scores
 
 
