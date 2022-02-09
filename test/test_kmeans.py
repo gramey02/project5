380847ff331cb2923fa2c98881e179c4779edac6
Grace Ramey <Grace.Ramey@ucsf.edu>
2022-02-09 15:12:12 -0800

    added to kmeans test

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,20 +1,27 @@
 #Importing Dependencies
 import pytest
 import numpy as np
-from cluster import (KMeans, Silhouette)
+from cluster import (KMeans, Silhouette, make_clusters)
 
-def test_edgeCase_clustering():
+def test_kmeans():
     #test things like k=0, samples<k, large dimensionality (m), low dimensionality (m=1), very high k
-    
-    pass
-
-
-def test_clustering():
     #check to make sure that a few points that should be clustered together are in fact assigned the same labels
-    pass
+    #check to make sure that k=0 raises an AttributeError
+    try:
+        kmeans = KMeans(k=0)
+        assert False
+    except AttributeError:
+        assert True
+        
+    
+    #check that it will run with a very large k
+    kmeans2 = KMeans(k=1000)
+    kmeans2.fit()
+    
 
 
-def test_silhouette_scoring():
+def test_silhouette():
     #check to make sure silhouette scores fall between -1 and 1
     #check that silhouette scores incr. as k increases (maybe compare avg silhouette score for the data in different k cases)
+    
     pass
